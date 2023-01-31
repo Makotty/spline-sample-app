9407cf09b1c81f2dba746bc7848e26c1bb957497
makotty <makotty.mako@gmail.com>
2023-01-31 21:46:36 +0900

    Show 3D Fps KeyBoard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import Spline from "@splinetool/react-spline";
+
 import "./App.css";
 
 const App = () => {
   return (
-    <>
-      <></>
-    </>
+    <div className="FpsKeyboard">
+      <Spline scene="https://prod.spline.design/Ec6djAuEt5dsHz4v/scene.splinecode" />
+    </div>
   );
 };
 
